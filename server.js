7a1d4a62df3aee0b52be5194541c6be2df458251
Joel essienobong <joddeilove@gmail.com>
2025-04-24 22:29:01 +0100

    nothing much

--- server.js
+++ server.js
@@ -23,6 +23,6 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is supplying on port ${PORT}`);
 });
 
