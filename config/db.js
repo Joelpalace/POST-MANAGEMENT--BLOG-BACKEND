b59af57a7ee108226d2e7dad66de4a9fb67e2a3c
Joel essienobong <joddeilove@gmail.com>
2025-04-28 10:18:12 +0100

    afresh

--- config/db.js
+++ config/db.js
@@ -1,16 +1,11 @@
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-
-dotenv.config();
-
+const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB connected");
+    console.log('MongoDB connected');
   } catch (err) {
     console.error(err.message);
     process.exit(1);
   }
 };
-
-export default connectDB;
+module.exports = connectDB;
--- models/Tag.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const TagSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true, lowercase: true },
-  slug: { type: String, required: true, unique: true }
-});
-
-export default mongoose.model("Tag", TagSchema);
