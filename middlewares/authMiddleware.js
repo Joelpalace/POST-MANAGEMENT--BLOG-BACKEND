aed4d31c13cac48560d038a6c1e5b2361f96eeba
Joel essienobong <joddeilove@gmail.com>
2025-04-30 10:53:20 +0100

    refactor: remove commented-out code from auth middleware for clarity

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,25 +1,3 @@
-/* const jwt = require('jsonwebtoken');
-const User = require('../models/User');
-exports.protect = async (req, res, next) => {
-  let token;
-  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-    token = req.headers.authorization.split(' ')[1];
-  }
-  if (!token) return res.status(401).json({ msg: 'Not authorized' });
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = await User.findById(decoded.id).select('-password');
-    next();
-  } catch (err) {
-    res.status(401).json({ msg: 'Invalid token' });
-  }
-};
-exports.isAdmin = (req, res, next) => {
-  if (req.user.role !== 'admin') return res.status(403).json({ msg: 'Forbidden' });
-  next();
-};
- */
-
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
